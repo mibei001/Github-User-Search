03a3a90032fc11b42bded2870ff3f5e978675023
Luther <lutherking30@gmail.com>
2021-10-19 11:18:57 +0300

    Added profile service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map } from  "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+private username:string;
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+console.log("service is active");
+this.username = "ro0tkit";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username)
+.map(res => res.json());
+  }
 }
+   
