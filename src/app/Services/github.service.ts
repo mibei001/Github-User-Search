b65f750e137be9a5b7567273e6b22928330c57b4
Luther <lutherking30@gmail.com>
2021-10-19 14:54:57 +0300

    Added User Profile component

---
+++ src/app/Services/github.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { Users } from '../users';
+import { Repos } from '../repos';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  user: Users;
+
+// tslint:disable-next-line: variable-name
+  api_Url: string = environment.apiUrl;
+  repos$;
+  constructor(private http: HttpClient) { }
+  searchGithubUser(username) {
+    // tslint:disable-next-line:class-name
+    interface userInfo {
+      login: string;
+      location: string;
+      followers: number;
+      following: number;
+      created_at: Date;
+      email: string;
+      avatar_url: any;
+      public_repos: number;
+    }
+    const promise = new Promise<void>(
+      (resolve, reject) => {
+        this.http
+          .get<userInfo>(environment.apiUrl + username + '/repos')
+          .toPromise()
+          .then(
+            response => {
+              this.user = new Users(
+                response.login,
+                response.location,
+                response.followers,
+                response.following,
+                response.created_at,
+                response.email,
+                response.avatar_url,
+                response.public_repos
+              );
+
+              // console.log(this.user)
+              console.log(response);
+
+              resolve();
+            },
+            error => {
+              console.log(error);
+            }
+          );
+      }
+    );
+    return promise;
+  }
+  searchGithubRepos(username) {
+
+    // tslint:disable-next-line:class-name
+    interface userInfo {
+       name: string;
+       forks: number;
+       watcher: number;
+    }
+
+    const promise = new Promise<void>(
+      (resolve, reject) => {
+        this.http.get<userInfo>(this.api_Url + username + '/repos')
+        .toPromise()
+        .then(
+          githubData => {
+            console.log(githubData[0]);
+            this.repos$ = githubData;
+            console.log(this.repos$);
+
+            resolve();
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    );
+    return promise;
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubNavComponent } from './github-nav/github-nav.component';
+import { GithubUserProfileComponent } from './github-user-profile/github-user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubNavComponent,
+    GithubUserProfileComponent,
   ],
   imports: [
     BrowserModule,
