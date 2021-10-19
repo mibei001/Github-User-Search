5da7d4eecce4f100ec2e4dd3829eb1d727b345da
Luther <lutherking30@gmail.com>
2021-10-19 15:02:56 +0300

    Added Users class

---
+++ src/app/users.ts
@@ -0,0 +1,13 @@
+export class Users {
+    constructor(public login: string,
+                public location: string,
+                public followers: number,
+                public following: number,
+                public memberSinceDate: Date,
+                public email: string,
+  // tslint:disable-next-line: variable-name
+                public avatar_url: any,
+                public repos) {
+  }
+  }
+  
