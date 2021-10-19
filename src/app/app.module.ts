dbf8b96653b3a9f31405b3ca7e8340dcec2b2696
Luther <lutherking30@gmail.com>
2021-10-19 14:47:57 +0300

    Added github navigation container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ProfileService } from "./profile.service";
-import { HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { GithubNavComponent } from './github-nav/github-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    GithubNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
   ],
-  providers: [ProfileService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/github-nav/github-nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../githubs/github.service';
+
+@Component({
+  selector: 'app-github-nav',
+  templateUrl: './github-nav.component.html',
+  styleUrls: ['./github-nav.component.css'],
+  providers: [GithubService]
+})
+export class GithubNavComponent implements OnInit {
+
+  constructor(public githubservice: GithubService) { }
+
+  searchUser($input) {
+    if ($input.target.value === '') {
+
+    } else {
+      this.githubservice.searchGithubUser($input.target.value);
+      this.githubservice.searchGithubRepos($input.target.value);
+    }
+    console.log($input.target.value);
+  }
+  ngOnInit(): void {
+  }
+
+}
